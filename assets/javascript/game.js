25ba9c0403113153ad97ec6e9d1434f7b5e64cfe
rhegipatricio <rhegi.patricio@gmail.com>
2017-05-24 06:35:30 -0400

    added jg.jpg to .DS_Store

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,20 @@
 <script type="text/javascript">
 
 
- //These are the variables
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var wins = 0;
-var losses = 0;
-var left = 10;
-var soFar = [];
+	//These are the variables
+	var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	var wins = 0;
+	var losses = 0;
+	var left = 10;
+	var soFar = [];
 
-
-
- //
-document.onkey = function (event){
+ 	//
+	document.onkey = function (event){
 	var userGuess = event.key;
 	soFar, textContent == event.key +", ";
 
-
-
 	soFar = document.getElementByID("soFar");
 	console.log(userGuess);
-
 
 	//
 	for (var i = 0; i < answer.length i++) {
